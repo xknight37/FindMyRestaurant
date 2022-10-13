a3d36c564139e361f5b82350d2462f8cc10c939b
xknight37 <kissatshore94@outlook.com>
2022-10-14 00:15:13 +0530

    formatted

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Restaurant = require('./models/restaurant.model');
-const dbConfig = require('./configs/db.config');
-const serverConfig = require('./configs/server.config');
+const Restaurant = require("./models/restaurant.model");
+const dbConfig = require("./configs/db.config");
+const serverConfig = require("./configs/server.config");
 
 // register middleware to read json body
 app.use(express.json());
@@ -18,18 +18,18 @@ mongoose.connect(dbConfig.DB_URL);
 //connnection object created
 const db = mongoose.connection;
 //check if error connecting to database
-db.on('error',()=>{
+db.on("error", () => {
     console.log("error connecting to database");
-})
+});
 
 // need to export the routes created
 
-require('./routes/restaurant.routes')(app);
+require("./routes/restaurant.routes")(app);
 
 /**
  * start the server
  */
 
-app.listen(serverConfig.PORT,()=>{
+app.listen(serverConfig.PORT, () => {
     console.log(`Server started at ${serverConfig.PORT}`);
-})
+});
